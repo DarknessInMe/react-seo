d3e68936bbb238616672ac194a3055ae139fbca1
darknessInMe <elitniistalker@gmail.com>
2022-05-30 10:03:03 +0300

    fix content

--- src/Users.jsx
+++ src/Users.jsx
@@ -10,7 +10,7 @@ export const Users = () => {
                 <meta property='og:description' content={'Lorem ipsum'} />
                 <meta property='og:locale' content='en_US' />
                 <meta property='og:type' content='website' />
-                <meta property='og:url' content='https://patterns.dev/img/reactjs/react-logo@3x.svg' />
+                <meta property='og:image' content='https://patterns.dev/img/reactjs/react-logo@3x.svg' />
             </Helmet>
             <div>Users page</div>
         </div>
